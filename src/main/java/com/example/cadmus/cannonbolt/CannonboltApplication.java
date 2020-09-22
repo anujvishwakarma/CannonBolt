21b6482fc49c12eb90d6280effb33599b3b3ebc1
Anuj Vishwakarma <anvishwa@adobe.com>
2020-09-22 20:23:40 +0530

    Spring Init

---
+++ src/main/java/com/example/cadmus/cannonbolt/CannonboltApplication.java
@@ -0,0 +1,13 @@
+package com.example.cadmus.cannonbolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CannonboltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CannonboltApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cadmus/cannonbolt/CannonboltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadmus.cannonbolt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CannonboltApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
